db04c2fbdfec185d9d5047774f99e894271bfb98
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2020-11-28 17:46:05 +0000

    created Room class

--- models/room.py
+++ models/room.py
@@ -1,2 +1,6 @@
 class Room:
-    def __init__(self, name, capacity)
+    def __init__(self, name, capacity, id = None ):
+        self.name = name
+        self.capacity = capacity 
+        self.guests = []
+        self.id = id 
