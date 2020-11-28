33fb605fd2f608fa9951a88682aa2f53fa9545f4
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2020-11-28 16:57:08 +0000

    created and populated database

---
+++ db/tavern.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS rooms; 
+DROP TABLE IF EXISTS guests;
+DROP TABLE IF EXISTS weapons;
+
+CREATE TABLE weapons(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    stats VARCHAR(255)
+);
+
+CREATE TABLE guests(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    class VARCHAR(255),
+    race VARCHAR(255),
+    weapon_id INT REFERENCES weapons(id)
+);
+
+CREATE TABLE rooms(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    capacity INT,
+    guest_id INT REFERENCES guests(id)
+);
+
