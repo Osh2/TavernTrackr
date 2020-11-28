811cbbbc80368cf6fc24c4b53b27e0dd2401ce4c
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2020-11-28 19:20:01 +0000

    wrote and passed weapon stat test

--- tests/weapon_test.py
+++ tests/weapon_test.py
@@ -7,3 +7,9 @@ class TestWeapon(unittest.TestCase):
 
     def test_weapon_has_name(self):
         self.assertEqual('Orc Bane', self.weapon1.name)
+
+    def test_weapon_has_stats(self):
+        self.assertEqual(8, self.weapon1.stats['damage'])
+        self.assertEqual(250, self.weapon1.stats['value'])
+        self.assertEqual(True, self.weapon1.stats['magic'])
+        self.assertEqual('short sword', self.weapon1.stats['type'])
