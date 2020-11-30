3bbc43955c8dea1f3f54d8225db27e8c31b14c58
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2020-11-30 16:03:38 +0000

    added create guest functionality

--- controllers/guests_controller.py
+++ controllers/guests_controller.py
@@ -1,16 +1,28 @@
 from flask import Flask, Blueprint, render_template, request, redirect
 from models.guest import Guest
 import repositories.guest_repository as guest_repository
+import repositories.room_repository as room_repository
 
 guests_blueprint = Blueprint("guests", __name__)
 
 @guests_blueprint.route("/guests")
 def guests():
     guests = guest_repository.read_all()
-    return render_template("guests/index.html", guests = guests)
+    rooms = room_repository.read_all()
+    return render_template("guests/index.html", guests = guests, rooms=rooms)
 
 @guests_blueprint.route("/guests/<id>/delete", methods=["POST"])
 def delete_guest(id):
     guest_repository.delete(id)
     return redirect("/guests")
     
+@guests_blueprint.route("/guests", methods=["POST"])
+def add_guest():
+    guest_name = request.form["name"]
+    guest_type = request.form["type"]
+    guest_race = request.form["race"]
+    room = request.form["room_id"]
+    guest_room = room_repository.read(room)
+    guest = Guest(guest_name, guest_type, guest_race, guest_room)
+    guest_repository.save(guest)
+    return redirect("/guests")
