0b0bab14b0bee964b01a27a17f363e693995b97e
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2020-12-01 13:29:08 +0000

    added edit guest feature

--- controllers/guests_controller.py
+++ controllers/guests_controller.py
@@ -31,3 +31,29 @@ def add_guest():
 def get_guests_weapons(id):
     weapons = guest_repository.read_guests_weapons(id)
     return render_template("guests/weapons.html", weapons=weapons)
+
+@guests_blueprint.route("/guests/<id>/edit")
+def edit_guest_page(id):
+    guest = guest_repository.read(id)
+    rooms = room_repository.read_all()
+    return render_template("guests/edit.html", guest = guest, rooms=rooms)
+
+@guests_blueprint.route("/guests/<id>", methods = ["POST"])
+def update_guest(id):
+    name = request.form["name"]
+    type = request.form["type"]
+    race = request.form["race"]
+    room_id = request.form["room_id"]
+    room = room_repository.read(room_id)
+    guest = Guest(name, type, race, room, id)
+    guest_repository.update(guest)
+    return redirect("/guests")
+
+
+#     @rooms_blueprint.route("/rooms/<id>", methods = ["POST"])
+# def update_room(id):
+#     name = request.form["name"]
+#     capacity = request.form["capacity"]
+#     room = Room(name, capacity, id)
+#     room_repository.update(room)
+#     return redirect("/rooms")
